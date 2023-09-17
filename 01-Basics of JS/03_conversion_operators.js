3c6c33cf580e3393ffec368e0dfcea3f03dd3b10
Abdulhaseebimran <haseebimran0001@gmail.com>
2023-09-17 18:52:15 +0500

    operators in JS

---
+++ 01-Basics of JS/03_conversion_operators.js
@@ -0,0 +1,74 @@
+// conversion operators
+
+let score = "57";
+
+// console.log(typeof score); // first method to check types 
+// console.log(typeof (score)); // 
+
+let valueInNumber = Number(score); // to convert the value in number
+// console.log(typeof valueInNumber); // for check types
+// console.log(valueInNumber); // for check value
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLogin = "Abdul";
+
+let BooleanIsLogin = Boolean(isLogin);
+// console.log(BooleanIsLogin);
+
+// 1 => true; 0 => false
+// "" => false
+// "Abdul" => true
+
+//  string
+
+let someNumber = 57;
+
+let stringSomeNumber = String(someNumber); // to convert the value in string
+// console.log(typeof stringSomeNumber);  // for check types
+// console.log(stringSomeNumber); // for check value
+
+
+// *********************** Operations ***********************//
+
+let valueIn = 57;
+let NegValue = -valueIn;
+// console.log(NegValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// string concatenation
+
+let firstName = "Abdul";
+let lastName = "Haseeb";
+
+let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
