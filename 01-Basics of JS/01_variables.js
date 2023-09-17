9f6a6a627121f4481600d90724dd40c846d2c11a
Abdulhaseebimran <haseebimran0001@gmail.com>
2023-09-17 18:50:55 +0500

    variables in JS

---
+++ 01-Basics of JS/01_variables.js
@@ -0,0 +1,19 @@
+console.log("Abdul Haseeb");
+
+const accountId = 1245;
+let acountName = "Abdul Haseeb";
+var accountType = "Saving";
+accountCurrency = "PKR";
+
+// accountId = 1234;  // not allowed to change the value of constant variable
+
+// console.log(accountId);
+
+// change value of above variables
+
+acountName = "Abdul Haseeb Imran";
+accountType = "Current";
+accountCurrency = "USD";
+
+
+console.table([accountId, acountName, accountType, accountCurrency]);
