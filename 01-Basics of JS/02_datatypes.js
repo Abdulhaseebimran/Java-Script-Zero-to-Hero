f643a21fac3b0684ec37ec694e3545040e168f9c
Abdulhaseebimran <haseebimran0001@gmail.com>
2023-09-17 18:51:38 +0500

    datatypes in JS

---
+++ 01-Basics of JS/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat this file as a newer version of JavaScript
+
+
+// alert("Hello"); // it's shows the message in the browser
+
+console.log("Hello World"); // it's shows the message in the console
+
+let name = "Abdul Haseeb";
+let age = 20;
+let isLogin = true;
+
+console.table({name, age, isLogin}); // it's shows the message in the table format
+
+
+// Data Types
+// 1. Primitive Data Types
+// number = 2 , 2.5 , 3.14
+// string = "Hello World"
+// boolean = true , false
+// null = null  not exist
+// undefined = undefined  declare but not assign
+// symbol = Symbol()  unique value
+// BigInt = 1234567890123456789012345678901234567890n  large number
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
