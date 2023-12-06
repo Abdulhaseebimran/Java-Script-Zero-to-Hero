ce16e2be25307d5b8f021efcbd22c5ee3e0386c4
Abdulhaseebimran <haseebimran0001@gmail.com>
2023-12-06 14:46:45 +0500

    IIFE function

---
+++ 03-Basics/04_iife.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    // named IIFE
+     console.log("Chai is a tea");
+})();
+
+// simple IIFE with arrow function and parameter or arguments
+
+((name) => {
+    console.log(`your name is : ${name} `);
+})("Abdul Haseeb");
